7642ad6dd01d38468fa1c55ce73e19ce260d1170
vc7 <vince78718@gmail.com>
2017-02-03 23:11:43 +0900

    Initial Commit. CocoaPods prepared.

---
+++ Podfile
@@ -0,0 +1,13 @@
+target 'near' do
+  use_frameworks!
+
+	# General Usage
+	pod 'Alamofire'
+	pod 'SwiftyJSON'
+
+  target 'nearTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ near/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  near
+//
+//  Created by vincent on 2017/02/03.
+//  Copyright © 2017年 vc7. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        return true
+    }
+}
+
