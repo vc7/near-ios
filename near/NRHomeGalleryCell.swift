63d09b2f1183682627817e2ccb92053f71a061fb
vc7 <vince78718@gmail.com>
2017-02-05 16:40:08 +0900

    Add `NRHomeGalleryPhotoCell`, finished the basic layout of Gallery section.

--- near/NRHomeGalleryCell.swift
+++ near/NRHomeGalleryCell.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
+fileprivate let kNRHomeGalleryPhotoCellIdentifier = "NRHomeGalleryPhotoCellIdentifier"
+
 /// The cell to be display the nearby photos.
 class NRHomeGalleryCell: UITableViewCell {
 
-    static let defaultHeight: CGFloat = 250
+    /// The default ideal height for the instance of `NRHomeGalleryCell`
+    static let defaultHeight: CGFloat = 265
+    
     @IBOutlet internal weak var collectionView: UICollectionView!
     
     override func awakeFromNib() {
@@ -19,6 +23,8 @@ class NRHomeGalleryCell: UITableViewCell {
         
         self.selectionStyle = .none
         
+        self.collectionView.register(UINib.init(nibName: "NRHomeGalleryPhotoCell", bundle: nil), forCellWithReuseIdentifier: kNRHomeGalleryPhotoCellIdentifier)
+        self.collectionView.showsHorizontalScrollIndicator = false
         self.collectionView.dataSource = self
         self.collectionView.delegate = self
     }
@@ -27,14 +33,28 @@ class NRHomeGalleryCell: UITableViewCell {
 // MARK: - UICollectionViewDataSource
 extension NRHomeGalleryCell: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return 10
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kNRHomeGalleryPhotoCellIdentifier, for: indexPath) as! NRHomeGalleryPhotoCell
+        
+        return cell
     }
 }
 
 // MARK: - UICollectionViewDelegate
 extension NRHomeGalleryCell: UICollectionViewDelegate {
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension NRHomeGalleryCell: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return NRHomeGalleryPhotoCell.defaultSize
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsetsMake(0, 40, 0, 40)
+    }
 }
---
+++ near/NRHomeGalleryPhotoCell.swift
@@ -0,0 +1,25 @@
+//
+//  NRHomeGalleryPhotoCell.swift
+//  near
+//
+//  Created by vincent on 2017/02/05.
+//  Copyright © 2017年 vc7. All rights reserved.
+//
+
+import UIKit
+
+/// The cell class to display a single photo in `NHRHomeGallery`'s collection view.
+class NRHomeGalleryPhotoCell: UICollectionViewCell {
+
+    /// The default ideal height for the instance of `NRHomeGalleryCell`
+    static let defaultSize: CGSize = CGSize(width: 150.0, height: 185.0)
+    
+    @IBOutlet internal weak var photoImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
