488e758f8e6bb3ea3b88f4c919e1348927a691db
vc7 <vince78718@gmail.com>
2017-02-06 00:23:20 +0900

    Add API Location's route.

---
+++ near/LHNetworkRouteType+NR.swift
@@ -0,0 +1,13 @@
+//
+//  LHNetworkRouteType+NR.swift
+//  near
+//
+//  Created by vincent on 2017/02/05.
+//  Copyright © 2017年 vc7. All rights reserved.
+//
+
+import LHNetworkBase
+
+extension LHNetworkRouteType {
+    var config: LHNetworkConfigurationType { return NRNetworkConfiguration.default }
+}
