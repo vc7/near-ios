68a6a47a4bf0142a1ca4e686d194e00230d1059b
vc7 <vince78718@gmail.com>
2017-02-06 20:06:31 +0900

    Adjust temperature model.

--- near/NRNTemperature.swift
+++ near/NRNTemperature.swift
@@ -10,15 +10,15 @@ import LHNetworkBase
 import SwiftyJSON
 
 struct NRNTemperature {
-    var current: Float?
-    var high: Float?
-    var low: Float?
+    var current: String?
+    var high: String?
+    var low: String?
 }
 
 extension NRNTemperature: LHNJSONDecodable {
     init?(json: JSON) {
-        self.current = json["current"].float
-        self.high = json["high"].float
-        self.low = json["low"].float
+        self.current = json["current"].string
+        self.high = json["high"].string
+        self.low = json["low"].string
     }
 }
