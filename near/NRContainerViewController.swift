a3f85fb54c5857762d46d954eff5009399a00e66
vc7 <vince78718@gmail.com>
2017-02-06 20:47:46 +0900

    Add container view controller's basic functionality.

--- near/NRContainerViewController.swift
+++ near/NRContainerViewController.swift
@@ -20,12 +20,41 @@ class NRContainerViewController: UIViewController {
     /// The current page status of this container view controller.
     var status: NRContainerViewControllerStatus = .default
     
+    var currentConstraints: [NSLayoutConstraint] = []
     var currentViewController: UIViewController? {
         willSet {
             // TODO: remove old one
+            if let viewController = currentViewController {
+                self.view.removeConstraints(self.currentConstraints)
+                self.currentConstraints = []
+                
+                viewController.removeFromParentViewController()
+                viewController.view.removeFromSuperview()
+                viewController.didMove(toParentViewController: nil)
+            }
         }
         didSet {
             self.updateStatus()
+            
+            if let viewController = currentViewController {
+                self.addChildViewController(viewController)
+                
+                viewController.loadViewIfNeeded()
+                let childView = viewController.view!
+                childView.translatesAutoresizingMaskIntoConstraints = false
+                
+                self.view.addSubview(childView)
+                
+                self.currentConstraints = [
+                    NSLayoutConstraint(item: childView, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 0),
+                    NSLayoutConstraint(item: childView, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1, constant: 0),
+                    NSLayoutConstraint(item: childView, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 0),
+                    NSLayoutConstraint(item: childView, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: 0)
+                ]
+                self.view.addConstraints(self.currentConstraints)
+                
+                viewController.didMove(toParentViewController: self)
+            }
         }
     }
     
